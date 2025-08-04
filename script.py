7da09e2af4f5e5bcdfbca8e93c35119c2cf9a949
thechristoth <64496218+thechristoth@users.noreply.github.com>
2025-08-04 16:20:54 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-peintf("Py Script Runs!")
 with open("output.md", "w") as f:
     f.write(f"# Daily Report\n\nGenerated on {datetime.utcnow()} UTC\n")
     f.write("- Everything is running smoothly!\n")
