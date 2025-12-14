63f217797344047058112e9bda82bc0be4448305
thechristoth <64496218+thechristoth@users.noreply.github.com>
2025-12-14 11:07:30 +0100

    Update script.py

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ STOCKS = [
     "NVO", "WING", "EXLS", "EEFT", "AX", "UHS", "EPAM", "ICLR", "GHC", "SAIA",
     "RACE", "WDFC", "CW", "WAT", "CLH", "HCI", "NSSC", "NMIH", "CVCO", "IRMD",
     "AWK", "DXCM", "SPSC", "POOL", "FERG", "CACI", "LDOS", "DDOG", "MNDY", "NET",
-    "DECK", "LULU", "CROX", "ERIE", "ULTA", "UBER", "NVR", "ESQ", "CPRX", "EME"
+    "DECK", "LULU", "CROX", "ERIE", "ULTA", "UBER", "NTES", "ESQ", "CPRX", "EME"
 ]
 
 
